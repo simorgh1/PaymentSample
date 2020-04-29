5fd0f90a397df5d9cd4a13d6b298b39190f29e9d
Bahram Maravandi <simorgh1@github.com>
2020-04-29 12:23:14 +0200

    Extended ExpiryValidator.

--- Bank/src/Bank.Core/Validators/ExpiryValidator.cs
+++ Bank/src/Bank.Core/Validators/ExpiryValidator.cs
@@ -6,6 +6,7 @@ namespace Bank.Core.Validators
 {
     public static class ExpiryValidator
     {
+        private const int Millenium = 2000;
         /// <summary>
         /// Basic validation of the expiry for a given credit card
         /// It must be in mm/yy format , which is 2 digit for month and 2 digit for year separated by /.
@@ -28,6 +29,17 @@ namespace Bank.Core.Validators
 
             if (!Check.IsNumber(expiryDigits[0]) ||
                 !Check.IsNumber(expiryDigits[1]))
+                return false;
+
+            int expiryMonth = int.Parse(expiryDigits[0]);
+            int expiryYear = int.Parse(expiryDigits[1]) + Millenium;
+
+            if (expiryMonth < 1 || expiryMonth > 12)
+                return false;
+
+            int thisYear = DateTime.UtcNow.Year;
+
+            if (expiryYear < thisYear || expiryYear > thisYear + 10)
                 return false;
 
             return true;
